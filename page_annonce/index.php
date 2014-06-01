ff06decb39dcd066a7ea65e21400bea96151534f
perlesteiger <perlesteiger@aol.com>
2014-06-01 14:22:30 +0200

    ajout doc

---
+++ page_annonce/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<HTML>
+	<HEAD>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		
+		<title>SwimLap</title>
+		
+		<link rel="shortcut icon" href="favicon.ico">
+		<style type="text/css"> 
+			#content { 
+				background-image: url('index.png');
+				width: 1200px;
+				height: 900px;
+				margin: auto;
+				background-repeat: no-repeat;
+			}
+		</style> 
+	</HEAD>
+    <BODY>
+		<div id="content"></div>
+    </BODY>
+</HTML>
